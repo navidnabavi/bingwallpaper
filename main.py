bf25cf07b665e1055bc39c4a83502df4d76f8ab1
Navid Nabavi <navid92@gmail.com>
2021-01-04 14:54:32 +0330

    fix python3 support problem, black style format

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from requests import get
 import os
 import getpass
 
-api_url = 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
-image_path = '/home/{}/wallpaper.jpg'.format(getpass.getuser())
-image_url = 'http://www.bing.com/{}_1920x1080.jpg'
+api_url = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
+image_path = "/home/{}/wallpaper.jpg".format(getpass.getuser())
+image_url = "http://www.bing.com/{}_1920x1080.jpg"
 image_request = get(api_url).json()
 
-image = get(image_url.format(image_request['images'][0]['urlbase'])).content
+image = get(image_url.format(image_request["images"][0]["urlbase"])).content
 
-file = open(image_path, 'w')
+file = open(image_path, "wb")
 file.write(image)
 file.close()
 
