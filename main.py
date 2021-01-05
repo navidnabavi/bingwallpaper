7f1691010f96b0143a084bd1dc4254b380ebc8c2
Ashkan <a.farhadi@pishro.computer>
2021-01-05 15:59:18 +0330

    Deleted constants file

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from requests import get
 import os
-import constants
+import requests
 
-image_url = "http://www.bing.com/{}_1920x1080.jpg"
-image_request = get(constants.API_URL).json()
+image_url = os.getenv("IMAGE_URL")
+image_request = requests.get(os.getenv("API_URL")).json()
 
-image = get(image_url.format(image_request["images"][0]["urlbase"])).content
+image = requests.get(image_url.format(image_request["images"][0]["urlbase"])).content
 
-with open(constants.IMAGE_PATH, "wb") as f:
+with open(os.getenv("IMAGE_PATH"), "wb") as f:
     f.write(image)
 
 command = "gsettings set org.mate.background picture-filename '{}'".format(
-    constants.IMAGE_PATH
+    os.getenv("IMAGE_PATH")
 )
 
 os.system(command)
